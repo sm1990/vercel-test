b8af053a3769f50dc14d0d2b85c5a1cfb8d09c78
Sarani Mendis <saranimendisb@gmail.com>
2023-04-07 14:28:53 +0530

    remove csp

--- next.config.js
+++ next.config.js
@@ -1,29 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const securityHeaders = [
-  {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN',
-  },
-  {
-    key: 'Content-Security-Policy',
-    value: "frame-ancestors 'none';"
-  }
-]
+
 
 
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/:path*',
-        headers: securityHeaders,
-      },
-    ]
   },
 }
 
