b89faed433d720c65096a6897bfee45f0074dfdc
Sarani Mendis <saranimendisb@gmail.com>
2023-04-18 17:56:50 +0530

    add meta

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
+    <Head>
+    <META HTTP-EQUIV='Content-Security-Policy' CONTENT="frame-ancestors 'none';"/>
+    </Head>
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
@@ -98,5 +103,6 @@ export default function Home() {
         </a>
       </div>
     </main>
+    </>
   )
 }
